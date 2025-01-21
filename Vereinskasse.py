55ef0b8b0a52f122a6fd14f8decbc1a8d2136ac0
Leosch0403 <leonardschmid2804@gmail.com>
2025-01-21 16:36:59 +0100

    new file

---
+++ Vereinskasse.py
@@ -0,0 +1,18 @@
+import csv
+
+class Sytem:
+
+    class Accounts:
+
+    class New_user:
+
+
+
+        class Authentification:
+
+            def __init__(self, name, password):
+                self.name = name
+                self.password = password
+
+            def login(self, name, password):
+                if
