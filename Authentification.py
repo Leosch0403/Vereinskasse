b385e4bd40dd696241da88f0f4c844b135fd7d4b
Leosch0403 <leonardschmid2804@gmail.com>
2025-01-28 03:10:04 +0100

    Fertigstellung des Login

--- Authentification.py
+++ Authentification.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from User_verwaltung import *
+from tkinter import messagebox
 
 # Init main frame
 root = tk.Tk()
@@ -6,10 +8,37 @@ root.title('Startbildschirm')
 root.geometry('400x300')
 root.configure(background='white')
 
-def info():
-    name = name_entry.get()
-    print(f"Your name is {name}")
+admin_name = ['Hannes', 'Anna']
+admin_password = ['p_0815', 'anna_03']
 
+admin_lst = []
+# Erstellen eines neuen Auto-Objekts und Hinzufügen zur Liste
+for name, password in zip(admin_name, admin_password):
+    neuer_admin = Administrator(name, password)
+    admin_lst.append(neuer_admin)
+
+def authentificator():
+    input_name = name_entry.get()
+    input_password = password_entry.get()
+
+    name_count = False
+    password_count = False
+    for obj in admin_lst:
+        if input_name == obj._name:
+            name_count = True
+        if input_password == obj._password:
+            password_count = True
+
+        if input_password == obj._password and input_name == obj._name:
+            messagebox.showinfo('Authentifizierung erfolgreich', f"Willkommen {obj._name}, du bist {obj.role}")
+
+    if not name_count and not password_count:
+        print(f"Der eingegebene Benutzername {input_name} "
+              f"und Passwort {input_password} existieren nicht")
+    elif not name_count:
+        print(f"Der eingegebene Benutzername {input_name} existiert nicht")
+    elif not password_count:
+        print(f"Das eingegebene Passwort {input_password} ist falsch")
 
 # Entry field for name
 name_label = tk.Label(root,
@@ -37,7 +66,7 @@ password_entry.pack()
 start_button = tk.Button(root,
                          font= 'Arial',
                          text= 'Eingabe bestätigen',
-                         command= info)
+                         command= authentificator)
 start_button.pack(pady= 10)
 
 root.mainloop()
